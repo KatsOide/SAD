abb272a01d709cb251ffa6cc48a13a370c20ff7c
KatsOide <oide1@icloud.com>
2017-07-04 13:44:38 +0200

    V1.1.0.9k64a
    1. Improved the closed-orbit finding including the determination of the
    origin of the rf phase “trf0” with  RADCOD.

--- src/MAIN.f
+++ src/MAIN.f
@@ -1,8 +1,8 @@
       module version
         character*19, parameter ::
 c                     /'1234567890123456789'/
-     $     versionid  ='1.1.0.8.1k64       ',
-     $     versiondate='6/28/2017 18:00:00 '
+     $     versionid  ='1.1.0.9k64a        ',
+     $     versiondate='7/5/2017 00:00:00  '
         character*25 builtdate
         character*30 startdat
       end module
