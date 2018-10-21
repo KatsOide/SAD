4c1be636819c2d2fb850b25656f0faebc86ab639
KatsOide <oide1@icloud.com>
2018-10-21 23:49:43 +0200

    gdtoa adjustments

--- src/gdtoa/gmisc.c
+++ src/gdtoa/gmisc.c
@@ -1,1 +1,1 @@
-/Users/oide/SAD/oldsad/contrib/gdtoa/gmisc.c
+/home/oide/SAD/oldsad/contrib/gdtoa/gmisc.c
--- src/gdtoa/misc.c
+++ src/gdtoa/misc.c
@@ -1,1 +1,1 @@
-/Users/oide/SAD/oldsad/contrib/gdtoa/misc.c
+/home/oide/SAD/oldsad/contrib/gdtoa/misc.c
