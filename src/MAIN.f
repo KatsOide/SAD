46a57b2400bbdc008d7faa719876204ca2d0e2cf
KatsOide <oide1@icloud.com>
2019-05-04 11:16:34 +0200

    Adjustments

--- src/MAIN.f
+++ src/MAIN.f
@@ -1,8 +1,8 @@
       module version
         character*19, parameter ::
 c                     /'1234567890123456789'/
-     $     versionid  ='1.1.6.6.3k64       ',
-     $     versiondate='3/30/2018 22:00:00 '
+     $     versionid  ='1.1.6.6.4k64       ',
+     $     versiondate='4/1/2018 23:00:00  '
 
         character*25 builtdate
         character*30 startdat
--- src/tffindroot_conflict-20190319-042708.f
+++ src/tffindroot_conflict-20190319-042708.f
@@ -99,7 +99,6 @@ c      write(*,*)'findroot-D ',used
           call tflocal1(ke%k)
           go to 9000
         endif
-c        call tfdebugprint(ke,'tffindroot-deriv',1)
       endif
       call tfnewton(ke%k,sav,v0,d0,kdl,
      $     vmin,vmax,neq,nvar,maxi,eps,trace,frac,irtc)
@@ -542,7 +541,6 @@ c        call tfstk2l(lista,list)
       kdl(1:nvar)=ktfoper+mtfnull
       if(used)then
         call tfderiv(ke,nvar,sav,kdl,irtc)
-c        call tfdebugprint(ke,'tffit-deriv',1)
       endif
       call tflocald(symdv%value)
       symdv%value%k=0
@@ -881,7 +879,6 @@ c          enddo
       do i=1,m
         rlist(kavvec+i)=a(1,i)
       enddo
-c     call tfdebugprint(ke,'ke',1)
       call tfeevalref(ke,kx,irtc)
       call tflocal1(kaxvec)
       symdv%value%k=0
