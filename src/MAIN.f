5f31b98800126885cb6c21eb26f209a579d91145
KatsOide <oide1@icloud.com>
2025-03-15 13:01:34 +0100

    Changes
    1. The exponent label of FrameTicks is displayed having less conflict with Tick labels.

--- src/MAIN.f
+++ src/MAIN.f
@@ -1,7 +1,7 @@
       module version
         character*19, parameter ::
 c                     /'1234567890123456789'/
-     $     versionid  ='1.3.6.3k64         ',
+     $     versionid  ='1.3.6.3.1k64       ',
      $     versiondate='3/20/2025 18:00:00 '
 
         character*25 builtdate
