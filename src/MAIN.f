220e54d803d4cc7b3eb41a9d68086c8b6392befc
KatsOide <oide1@icloud.com>
2026-03-08 13:00:36 +0100

    Adjust

--- src/MAIN.f
+++ src/MAIN.f
@@ -1,8 +1,13 @@
       module version
         character*19, parameter ::
 c                      /'1234567890123456789'/
+<<<<<<< HEAD
      $     versionid  = '1.4.7k64           ',
      $     versiondate= '3/10/2025 10:00:00 '
+=======
+     $     versionid  = '1.4.6k64           ',
+     $     versiondate= '12/31/2025 10:00:00'
+>>>>>>> origin/devel
 
         character*25 builtdate
         character*30 startdat
